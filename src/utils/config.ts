0ddd4afabd350d3bdb0fcfb43748b8278b1c26d1
Juan Pablo Sanchez Villegas <juanapablo@yconsultores.com>
2024-06-02 12:22:42 -0500

    new url

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,6 +1,3 @@
 export const prod = process.env.NODE_ENV ?? process.env.NEXT_PUBLIC_NODE_ENV;
 
-
-export const url = prod === "production" ? "http://localhost/api" : "http://localhost:3000/api";
-
-
+export const url = "http://18.217.23.192:3000/api";
