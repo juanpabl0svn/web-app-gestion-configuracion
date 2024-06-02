e3050cb466b918a57fb8c1743be359951c4e8f87
Juan Pablo Sanchez Villegas <juanapablo@yconsultores.com>
2024-06-02 12:19:56 -0500

    solving problem of cors

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,6 +1,5 @@
 export const prod = process.env.NODE_ENV ?? process.env.NEXT_PUBLIC_NODE_ENV;
 
-console.log(prod)
 
 export const url = prod === "production" ? "http://localhost/api" : "http://localhost:3000/api";
 
