d2528d7fcaeb979ce43603d25f45c3316499cf8a
Juan Pablo Sanchez Villegas <juanapablo@yconsultores.com>
2024-06-02 12:29:33 -0500

    'done'

--- src/utils/POST.ts
+++ src/utils/POST.ts
@@ -1,9 +1,8 @@
 import axios from "axios";
+import { URL } from "./config";
 
 export default function POST(endpoint: string, body?: any | null) {
-  const url = process.env.NEXT_PUBLIC_API_URL ?? "http://localhost:3000/api";
-
-  return axios.post(url + endpoint, body, {
+  return axios.post(URL + endpoint, body, {
     withCredentials: true,
   });
 }
